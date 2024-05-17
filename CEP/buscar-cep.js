92bd374a52b74c0714c6cce325dbb8c156cd4e20
Ana Beatriz <ana.franciscatto4419@gmail.com>
2024-05-17 11:31:27 -0300

    tudo certo

--- CEP/buscar-cep.js
+++ CEP/buscar-cep.js
@@ -1,9 +1,9 @@
-// ScrollReveal().reveal(`.logo`, { duration: 2000 });
-// ScrollReveal().reveal(`.text__header`, {
-//     rotate: { x: 0, y: 80, z: 0 },
-//     duration: 2000
-// });
-// ScrollReveal().reveal(`.container`, { duration: 2000 });
+ScrollReveal().reveal(`.logo`, { duration: 2000 });
+ScrollReveal().reveal(`.text__header`, {
+    rotate: { x: 0, y: 80, z: 0 },
+    duration: 2000
+});
+ScrollReveal().reveal(`.container`, { duration: 2000 });
 
 // OBTER O VALOR DOS CAMPOS.
 const cepInput = document.querySelector('#cep');
